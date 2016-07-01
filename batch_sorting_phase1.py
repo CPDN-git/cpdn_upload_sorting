4efa1e1f49292336964e5930dd4f1c22ad0c011e
Peter Uhe <Peter.Uhe@ouce.ox.ac.uk>
2016-07-01 11:13:59 +0100

    added optional arguments for cleaning up or not and some error handling

--- batch_sorting_phase1.py
+++ batch_sorting_phase1.py
@@ -2,7 +2,15 @@
 import os, sys, time,glob
 
 
-def batch_sorting_phase1(incoming_folder,batch_folders,results_folder):
+def batch_sorting_phase1(incoming_folder,batch_folders,results_folder,sort_unknown=False,delete_closed=True):
+	#
+	# Optional arguments:
+	# sort_unknown: sort files that don't match the lists of open or closed batches into "unknown_batches" folder
+	#               For this option, the 'open_batches.txt' and 'closed_batches.txt'
+	#               need to be up to date otherwise files will be sorted incorrectly
+	#               Default is False
+	# delete_closed: delete files from closed batches
+	#               Default is True
 	print time.strftime("%Y/%m/%d %H:%M:%S") + " Starting batch_sorting_phase1.py\n"
 
 	open_batches=[]
@@ -23,40 +31,45 @@ def batch_sorting_phase1(incoming_folder,batch_folders,results_folder):
 	# Loop over zip files in incoming folder
 	for fpath in glob.glob(incoming_folder+'/*.zip'):
 		fname=os.path.basename(fpath)
-		# Split string up (assume has the format: hadam3p_eu_fs23_201412_1_d401_000000951_0_r1285010883_1.zip
-		str_split=fname.split('_')
-		fname_out=''
-		if str_split[-2][0]=='r':
-			batch=str_split[-5]
-			# recombine fname without random part
-			for i,part in enumerate(str_split):
-				if i != len(str_split)-2:
-					fname_out=fname_out+part+'_'
-			fname_out=fname_out[:-1] # remove final underscore
-		else:
-			# Old format without random string
-			batch=str_split[-4]
-			fname_out=fname
-		workunit_name=fname_out[:-len(str_split[-1])-1] # workunit name is fname without the '_1.zip'
-		# Check if workunit is from an open batch
-		if batch in open_batches:
-			batch_folder=results_folder+'/batch_'+batch
-			if not os.path.exists(batch_folder):
-				os.mkdir(batch_folder)
-			if not os.path.exists(batch_folder+'/in_progress/'):
-				os.mkdir(batch_folder+'/in_progress/')
-			if not os.path.exists(batch_folder+'/in_progress/'+workunit_name):
-				os.mkdir(batch_folder+'/in_progress/'+workunit_name)
-			os.rename(fpath,batch_folder+'/in_progress/'+workunit_name+'/'+fname_out)
-			print fname_out,batch
-		# Check if workunit is from a closed batch
-		elif batch in closed_batches:
-		# Delete this file
-			os.remove(fpath)
-			print 'deleting from closed batch',fname,batch
-		else:
-				os.rename(fpath,results_folder+'/unknown_batches/'+fname)
-				print fname,'unknown batch'
+		try:
+			# Split string up (assume has the format: hadam3p_eu_fs23_201412_1_d401_000000951_0_r1285010883_1.zip
+			str_split=fname.split('_')
+			fname_out=''
+			if str_split[-2][0]=='r':
+				batch=str_split[-5]
+				# recombine fname without random part
+				for i,part in enumerate(str_split):
+					if i != len(str_split)-2:
+						fname_out=fname_out+part+'_'
+				fname_out=fname_out[:-1] # remove final underscore
+			else:
+				# Old format without random string
+				batch=str_split[-4]
+				fname_out=fname
+			workunit_name=fname_out[:-len(str_split[-1])-1] # workunit name is fname without the '_1.zip'
+			# Check if workunit is from an open batch
+			if batch in open_batches:
+				batch_folder=results_folder+'/batch_'+batch
+				if not os.path.exists(batch_folder):
+					os.mkdir(batch_folder)
+				if not os.path.exists(batch_folder+'/in_progress/'):
+					os.mkdir(batch_folder+'/in_progress/')
+				if not os.path.exists(batch_folder+'/in_progress/'+workunit_name):
+					os.mkdir(batch_folder+'/in_progress/'+workunit_name)
+				os.rename(fpath,batch_folder+'/in_progress/'+workunit_name+'/'+fname_out)
+				print fname_out,batch
+			# Check if workunit is from a closed batch
+			elif batch in closed_batches:
+				if delete_closed:
+					# Delete this file
+					os.remove(fpath)
+					print 'deleting from closed batch',fname,batch
+			else:
+				if sort_unknown:
+					os.rename(fpath,results_folder+'/unknown_batches/'+fname)
+					print fname,'unknown batch'
+		except:
+			print "Error sorting file:",fname
 	print time.strftime("%Y/%m/%d %H:%M:%S") + " Finished batch_sorting_phase1.py"
 #####################
 
--- batch_sorting_phase2.py
+++ batch_sorting_phase2.py
@@ -2,7 +2,11 @@
 import os, sys, time, glob, gzip
 import xml.etree.ElementTree as ET
 
-def batch_sorting_phase2(batch_folders,results_folder,upload_base):
+def batch_sorting_phase2(batch_folders,results_folder,upload_base,cleanup_closed=False):
+	#
+	# Optional argument 'cleanup_closed: 
+	# Delete folders for 'failed' and 'in_progress' workunits for closed batches
+	#
 	print time.strftime("%Y/%m/%d %H:%M:%S") + " Starting batch_sorting_phase2.py\n"
 
 	open_batches=[]
@@ -33,61 +37,75 @@ def batch_sorting_phase2(batch_folders,results_folder,upload_base):
 			batch=os.path.basename(batch_path).split('_')[-1]
 		else:
 			continue
-		if batch in open_batches:
-			print "Sorting batch",batch
+		try:
+			if batch in open_batches:
+				print "Sorting batch",batch
 
-			# Get lists of successful and failed tasks
-			successful_tasks=[]
-			failed_tasks=[]
-			for batch_folder in batch_folders:
-				try:
-					for task in open(batch_folder+'/batch_'+batch+'_successful_wus'):
-						successful_tasks.append(task.strip())
-					for task in open(batch_folder+'/batch_'+batch+'_failed_wus'):
-						failed_tasks.append(task.strip())
-				except:
-					continue
+				# Get lists of successful and failed tasks
+				successful_tasks=[]
+				failed_tasks=[]
+				for batch_folder in batch_folders:
+					try:
+						for task in open(batch_folder+'/batch_'+batch+'_successful_wus'):
+							successful_tasks.append(task.strip())
+						for task in open(batch_folder+'/batch_'+batch+'_failed_wus'):
+							failed_tasks.append(task.strip())
+					except:
+						continue
 
-			# Create folders if needed
-			if len(successful_tasks)>0:
-				success_folder=os.path.join(batch_path,'successful')
-				if not os.path.exists(success_folder):
-					os.mkdir(success_folder)
-				# Set up gzipped text file for list of successful output files
-				f_success= gzip.open(os.path.join(batch_path,'batch_'+batch+'.txt.gz'), 'ab')
-			if len(failed_tasks)>0:
-				failed_folder=os.path.join(batch_path,'failed')
-				if not os.path.exists(failed_folder):
-					os.mkdir(failed_folder)
+				# Create folders if needed
+				if len(successful_tasks)>0:
+					success_folder=os.path.join(batch_path,'successful')
+					if not os.path.exists(success_folder):
+						os.mkdir(success_folder)
+					# Set up gzipped text file for list of successful output files
+					f_success= gzip.open(os.path.join(batch_path,'batch_'+batch+'.txt.gz'), 'ab')
+				if len(failed_tasks)>0:
+					failed_folder=os.path.join(batch_path,'failed')
+					if not os.path.exists(failed_folder):
+						os.mkdir(failed_folder)
 
-			# Loop over in progress tasks
-			for in_progress_task in  glob.glob(os.path.join(batch_path,'in_progress/*')):
-				taskname=os.path.basename(in_progress_task)
-				if taskname in successful_tasks:
-					# Check if there are the right number of upload files
-					ul_files=len(glob.glob(in_progress_task+'/*.zip'))
-					if ul_files==batch_ul_files[batch]:
-						print in_progress_task,'to',os.path.join(success_folder,taskname)
-						os.rename(in_progress_task,os.path.join(success_folder,taskname))
-						# Write list of output files to f_success text file
-						for zipname in glob.glob(os.path.join(success_folder,taskname,'*')):
-							f_success.write(upload_base+zipname[len(results_folder):]+'\n')
-					else:
-						print "Error, wrong number of output files for task",taskname,ul_files
-				elif taskname in failed_tasks:
-					print in_progress_task,'to',os.path.join(failed_folder,taskname)
-					os.rename(in_progress_task,os.path.join(failed_folder,taskname))
+				# Loop over in progress tasks
+				for in_progress_task in  glob.glob(os.path.join(batch_path,'in_progress/*')):
+					taskname=os.path.basename(in_progress_task)
+					if taskname in successful_tasks:
+						# Check if there are the right number of upload files
+						ul_files=len(glob.glob(in_progress_task+'/*.zip'))
+						if ul_files==batch_ul_files[batch]:
+							print in_progress_task,'to',os.path.join(success_folder,taskname)
+							os.rename(in_progress_task,os.path.join(success_folder,taskname))
+							# Write list of output files to f_success text file
+							for zipname in glob.glob(os.path.join(success_folder,taskname,'*')):
+								f_success.write(upload_base+zipname[len(results_folder):]+'\n')
+						else:
+							print "Error, wrong number of output files for task",taskname,ul_files
+					elif taskname in failed_tasks:
+						print in_progress_task,'to',os.path.join(failed_folder,taskname)
+						os.rename(in_progress_task,os.path.join(failed_folder,taskname))
 
-			# Cleanup successful tasks file
-			if len(successful_tasks)>0:
-				f_success.close()
+				# Cleanup successful tasks file
+				if len(successful_tasks)>0:
+					f_success.close()
 
-		elif batch in closed_batches:
-			print "batch",batch,"is already closed, skipping"
-			#TODO, could put in clean up of in_progress and failed workunits here. 
-		else:
-			print "batch",batch,"is unknown"
-
+			elif batch in closed_batches:
+				print "batch",batch,"is closed"
+				if cleanup_closed:
+					fail_path=os.path.join(batch_path,'failed')
+					if os.path.exists(fail_path):
+						print 'Cleaning up failed workunits in:',fail_path
+						shutil.rmtree(fail_path)
+					in_progress_path=os.path.join(batch_path,'in_progress')
+					if os.path.exists(in_progress_path):
+						print 'Cleaning up in progress workunits in:',in_progress_path
+						shutil.rmtree(in_progress_path)
+				else:
+					print "...skipping"
+			else:
+				print "batch",batch,"is unknown"
+		except Exception,e:
+			print "Error sorting batch",batch
+			print e
+	print time.strftime("%Y/%m/%d %H:%M:%S") + " Finished batch_sorting_phase2.py"
 #####################
 #
 # Script Setup
