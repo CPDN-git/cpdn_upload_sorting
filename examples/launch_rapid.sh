ce70c099e2cf3794490eab6675ab6d11f292b0f6
Sarah Sparrow <sarah.sparrow@oerc.ox.ac.uk>
2016-12-02 11:04:46 +0000

    Rapid watch examples

---
+++ examples/launch_rapid.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+### Environment Variables for batch sorting scripts
+#
+# location of lists of batches and successful/failed workunits
+export BATCH_LISTS_URLS=http://vorvadoss.oerc.ox.ac.uk/cpdnboinc_dev/download/batch_lists,http://climateapps2.oerc.ox.ac.uk/batch
+
+# Folder for sorted results
+export RESULTS_FOLDER=/storage/boinc/upload
+
+# Incoming folder where new uploads are put
+export INCOMING_FOLDER=/storage/incoming/uploader
+
+# (optional) temporary directory which the backup 'open_batches.txt' and 'closed_batches.txt' are saved to
+export TMPDIR=/home/cpdn/cpdn_upload_sorting/tmp
+
+# (optional) url for sorted files on the upload folder (goes into list of successful workunits as a 'wget' file)
+#UPLOAD_BASE_URL=http://upload3.cpdn.org/results
+
+# Directory which these scripts are cloned into
+export SCRIPTS_DIR=/home/cpdn/cpdn_upload_sorting
+
+# Directory for log files
+export LOG_DIR=/home/cpdn/cpdn_upload_sorting/log
+
+# Cleanup the data from closed batches 
+export CLEANUP_CLOSED_BATCHES=TRUE
+
+# Sort by project
+export SORT_BY_PROJECT=TRUE
+ 
+
+# Do the sorting (phase1: hourly, phase2: daily @ 7am)
+$SCRIPTS_DIR/batch_sorting_phase1.py >> $LOG_DIR/batch_sorting_phase1.log
+$SCRIPTS_DIR/batch_sorting_phase2.py >> $LOG_DIR/batch_sorting_phase2.log
+
