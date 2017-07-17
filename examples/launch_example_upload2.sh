32a35c2c356d153703a7855285fe72c7693d69c2
Sarah Sparrow <Sarah@Sarahs-MacBook-Air-5.local>
2017-07-17 13:23:42 +0100

    Updated upload2 launch example to sort be project

--- examples/launch_example_upload2.sh
+++ examples/launch_example_upload2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export BATCH_LISTS_URLS=http://vorvadoss.oerc.ox.ac.uk/cpdnboinc_dev/download/batch_lists,http://climateapps2.oerc.ox.ac.uk/batch
-export RESULTS_FOLDER=/storage/boinc/upload
+export RESULTS_FOLDER=/storage/boinc/project_results
 export INCOMING_FOLDER=/storage/incoming/uploader
 export TMPDIR=/home/cpdn
 export UPLOAD_BASE_URL=http://upload2.cpdn.org/results
 export CLEANUP_CLOSED_BATCHES=TRUE
+export SORT_BY_PROJECT=TRUE
 ./batch_sorting_phase1.py >> /var/www/projects/cpdn/log_anemoi/batch_sorting_phase1.log
 ./batch_sorting_phase2.py >> /var/www/projects/cpdn/log_anemoi/batch_sorting_phase2.log
