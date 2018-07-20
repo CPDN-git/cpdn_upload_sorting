7cf63c2dbd0009f72096e311aec67cc5f0a7d4a2
Sarah Sparrow <Sarah@skadi.oerc.ox.ac.uk>
2018-07-20 13:26:58 +0100

    Adding in the chmod of files

--- examples/launch_example_upload11.sh
+++ examples/launch_example_upload11.sh
@@ -12,16 +12,16 @@ export RESULTS_FOLDER=/storage/boinc/project_results
 export INCOMING_FOLDER=/storage/incoming/uploader
 
 # (optional) temporary directory which the backup 'open_batches.txt' and 'closed_batches.txt' are saved to
-export TMPDIR=/home/boinc/cpdn_upload_sorting/tmp
+export TMPDIR=/home/boinc-daemon/cpdn_upload_sorting/tmp
 
 # (optional) url for sorted files on the upload folder (goes into list of successful workunits as a 'wget' file)
 #UPLOAD_BASE_URL=http://upload11.cpdn.org/results
 
 # Directory which these scripts are cloned into
-export SCRIPTS_DIR=/home/boinc/cpdn_upload_sorting
+export SCRIPTS_DIR=/home/boinc-daemon/cpdn_upload_sorting
 
 # Directory for log files
-export LOG_DIR=/home/boinc/log
+export LOG_DIR=/home/boinc-daemon/log
 
 # Cleanup the data from closed batches 
 export CLEANUP_CLOSED_BATCHES=TRUE
@@ -32,4 +32,7 @@ export SORT_BY_PROJECT=TRUE
 
 # Do the sorting (phase1: hourly, phase2: daily @ 7am)
 $SCRIPTS_DIR/batch_sorting_phase1.py >> $LOG_DIR/batch_sorting_phase1.log
+
+chmod -R 775 $RESULTS_FOLDER
+
 $SCRIPTS_DIR/batch_sorting_phase2.py >> $LOG_DIR/batch_sorting_phase2.log
